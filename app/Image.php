6da4be7a25ca64977834de9101552f2223197f74
Oladosu Tayo <34566850+habib2013@users.noreply.github.com>
2020-04-30 23:09:49 +0100

    Changed the length
    I changed the length of image

--- app/Image.php
+++ app/Image.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
 
+    
+    
+    
     protected $table = 'images';
     protected $fillable = array('album_id','description','image');
 }
